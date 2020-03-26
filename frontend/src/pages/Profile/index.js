986a264df9fc3114670fe4fd5fc684bd2ecb6989
Leandro Souza <leandro.sa.sou@gmail.com>
2020-03-26 13:56:40 -0300

    Front-end da aplicação

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import './style.css';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+export default function Profile() {
+  
+  const [incidents, setIncidents] = useState([]);
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+  const history = useHistory();
+
+  // useEfect executa uma função sempre que o valor do segundo parâmetro (ongId) for alterado
+  useEffect(() => {
+
+    api.get( 'profile', {headers: {Authorization: ongId}} )
+      .then(response => setIncidents(response.data));
+  
+  }, [ongId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+      
+      // Remove o incidente da tela atualizando a váriavel incidents
+      setIncidents(incidents.filter(icd => icd.id !== id ));
+
+    } catch (error) {
+      alert('Erro ao deletar caso. Tente Novamente!')
+    }
+  }
+
+  async function handleLogout(id) {
+    localStorage.clear();
+    history.push('/');
+  }
+  
+  return (
+    <div className="profile-container">
+      
+      <header>
+        <img src={logoImg} alt="Be The Hero"/>
+        
+        <span>Bem vindo, {ongName}</span>
+        
+        <Link className="button" to="/incidents/new" >Cadastrar novo caso</Link>
+        
+        <button type="button" onClick={handleLogout}>
+          <FiPower color="#E02041" size={18} />
+        </button>
+      </header>
+
+      <main>
+        
+        <h1>Casos Cadastrados</h1>
+
+        <ul>
+          { 
+            incidents.map(
+              icd => (
+                <li key={icd.id} >
+                  <strong>CASO:</strong>
+                  <p>{icd.title}</p>
+
+                  <strong>Descrição:</strong>
+                  <p>{icd.description}</p>
+
+                  <strong>Valor:</strong>
+                  <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(icd.value)}</p>
+
+                  <button
+                    type="button"
+                    onClick={ () => handleDeleteIncident(icd.id) }
+                  >
+                    <FiTrash2 size={20} color="#a8a8b3" />
+                  </button>
+                </li>
+              )
+            )
+          }
+        </ul>
+      </main>
+
+    </div>
+  );
+}
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import './style.css';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+export default function Register() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUF] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    const ong = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    };
+    try {
+      const response = await api.post('ongs', ong);
+      alert(`Seu ID de acesso ${response.data.id}`);
+      history.push('/');
+    } catch (error) {
+      alert('Erro no cadastro. tente novamente!');
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        
+        <section>
+          <img src={logoImg} alt=""/>
+          <h1>Cadastro</h1>
+          <p>Faça se cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041"/>Não Tenho cadastro
+          </Link>
+        </section>
+
+        <form onSubmit={handleRegister} >
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}  
+          />
+          <input
+            placeholder="Whatsapp"
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+          />
+          
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={e => setCity(e.target.value)}  
+            />
+            <input
+              style={{ width:80 }}
+              placeholder="UF"
+              value={uf}
+              onChange={e => setUF(e.target.value)}
+            />
+          </div>
+          
+          <button type="submit" className="button">Cadastrar</button>
+
+        </form>
+      </div>
+    </div>
+  );
+}
