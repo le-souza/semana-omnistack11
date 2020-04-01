56660ba80e464cc12e1249563c10d28369758237
Leandro Souza <leandro.sa.sou@gmail.com>
2020-04-01 00:34:20 -0300

    removendo pasta duplicada

--- backend-bkp/src/utils/generateUniqueId.js
+++
@@ -1,7 +0,0 @@
-
-const crypto = require('crypto');
-
-module.exports = function generateUniqueId(bytes = 4) {
-  // Usando o Crypto para criar um ID único.
-  return crypto.randomBytes(bytes).toString('HEX');
-}
